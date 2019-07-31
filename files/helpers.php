74356d29995493585a216e8dc4fa40d7fcc9ba60
Amir Bashiri <amir2b@users.noreply.github.com>
2019-07-31 15:19:57 +0430

    Update helpers.php

--- files/helpers.php
+++ files/helpers.php
@@ -1,40 +1,41 @@
 <?php
 
-if (!function_exists('image_url')) {
+if (! function_exists('image_url')) {
 
     /**
-     * @param string $file
-     * @param int $width
-     * @param int $height
-     * @param bool $is_crop
+     * @param  string  $file
+     * @param  int  $width
+     * @param  int  $height
+     * @param  bool  $is_crop
      * @return string
      */
     function image_url($file, $width = 0, $height = 0, $is_crop = false)
     {
-		$root_path = config('image-fit.root_path', 'files');
-		
+        $root_path = config('image-fit.root_path', 'files');
+
         $file = trim($file, '/');
         if (empty($file)) {
-            if (!empty(config('image-fit.image_default')))
+            if (! empty(config('image-fit.image_default'))) {
                 $file = config('image-fit.image_default');
-            else
+            } else {
                 return url('vendor/image-fit/no-image.jpg');
+            }
         }
 
         $info = pathinfo($file);
 
-        if (count($info) == 4) {
-            if (substr($info['dirname'], 0, 6) == "{$root_path}/")
-                $file = config('image-fit.prefix') . '/' . substr($info['dirname'], 6) . "/{$info['filename']}";
-            elseif ($info['dirname'] == $root_path)
-                $file = config('image-fit.prefix') . "/{$info['filename']}";
-            else
-                $file = config('image-fit.prefix') . "/{$info['dirname']}/{$info['filename']}";
+        if (count($info) === 4 && in_array($info['extension'], ['png', 'jpg', 'jpeg', 'gif'])) {
+            if (strpos($info['dirname'], "{$root_path}/") === 0) {
+                $file = config('image-fit.prefix').'/'.substr($info['dirname'], 6)."/{$info['filename']}";
+            } elseif ($info['dirname'] === $root_path) {
+                $file = config('image-fit.prefix')."/{$info['filename']}";
+            } else {
+                $file = config('image-fit.prefix')."/{$info['dirname']}/{$info['filename']}";
+            }
 
-            return url($file . ($is_crop ? '_' : '-') . "{$width}x{$height}.{$info['extension']}");
+            return url($file.($is_crop ? '_' : '-')."{$width}x{$height}.{$info['extension']}");
         }
 
         return url($file);
     }
-
 }
